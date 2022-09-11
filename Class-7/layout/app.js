30521a63e0d918c1cd597532c2d1c7c8f700ab0e
Felicia <ffratescu@yahoo.com>
2022-09-11 19:36:10 +0300

    'udpade'

---
+++ Class-7/layout/app.js
@@ -0,0 +1,15 @@
+let myContent = document.getElementById('app');
+
+let message = 'hello everyone';
+ number = 70;
+int NewNumber= 100;
+
+let car = {
+    brand: ford
+    miles: 12000
+    fuelType: diesel
+}
+
+console.log('this is the console');
+
+myContent.innerHTML = message+NewMessage;

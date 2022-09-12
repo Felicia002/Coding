67b8b5243575acc64689fe3a8c81d1ea45210bfb
Felicia <ffratescu@yahoo.com>
2022-09-12 22:11:01 +0300

    'update'

--- Class-7/layout/app.js
+++ Class-7/layout/app.js
@@ -1,15 +1,19 @@
 let myContent = document.getElementById('app');
 
-let message = 'hello everyone';
- number = 70;
-int NewNumber= 100;
+let message = 70;
+message = 100;
 
-let car = {
-    brand: ford
-    miles: 12000
-    fuelType: diesel
+let myCar = {
+    brand: "ford",
+    miles: "12000",
+    fuelType: "diesel"
 }
 
-console.log('this is the console');
+console.log('this is the console', myCar);
 
-myContent.innerHTML = message+NewMessage;
+myContent.innerHTML = message;
+
+
+let myArray = ["apple", "orange", "pear", "banana"];
+console.warn(myArray)
+myContent.innerHTML = myArray[1];
